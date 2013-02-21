5309ef2c58ae0cab3cabf4f08ebf2918da2ab864
pes014 <pes014@bucknell.edu>
2013-02-21 17:37:49 -0500

    Messed with classification

--- Code/Analysis_System/pCell.py
+++ Code/Analysis_System/pCell.py
@@ -3,6 +3,7 @@ import ImageEnhance
 import ImageOps
 import ImageStat
 import numpy
+from pImgMgr import pImgMgr
 
 class pCell(object):
 
@@ -22,9 +23,9 @@ class pCell(object):
     def __init__(self, x, y, im):
         self.x = x
         self.y = y
-        self.im = im
+        self.im = im # pImgMgr
         self.celltype = pCell.UNCLASSIFIED
-        self.iw = 0
+        self.boundaries = (self.x, self.y, self.x + pCell.width, self.y + pCell.height)
 
 
     """Static method for setting height and width"""
@@ -36,49 +37,46 @@ class pCell(object):
     
     # This is where the math goes COLIN
     """Tell cell to classify itself"""
-    def classify(self):
+    def classify(self, Iw):
         #im = Image.open(self.filename).convert("L")
-        im = self.im.convert("L")
-        cell_bwimage = im.crop(self.boundaries())
+        #im = self.im.convert("L")
+        cell_bwimage = self.im.getBW().crop(self.boundaries)
 
-        Iw = self.iw
+        #Iw = self.iw
 
-        Tw = 30    # Lower gives more foreground
-        Tsig = 300 # Higher gives more Board
+        Tw = 2    # Lower gives more foreground
+        Tsig = 40 # Higher gives more Board
         I = ImageStat.Stat(cell_bwimage).mean[0]
         sig = ImageStat.Stat(cell_bwimage).stddev[0]
         #sigw = numpy.std(Iw)   #Ideally something like this
         sigw = 0.1  #Seems to work for this value
         
-        if abs(I-Iw)/(sig+sigw) < Tw and sig/sigw < Tsig:
+        if I > (Iw * 0.8)  and sig/sigw < Tsig:
             self.celltype = pCell.BOARD
             #print "BOARD CELL"
 
-        elif abs(I-Iw)/(sig+sigw) < Tw and sig/sigw >= Tsig:
+        elif I > (Iw * 0.8) and sig/sigw >= Tsig:
             self.celltype = pCell.STROKE
             #print "STROKE CELL"
 
         else:
             self.celltype = pCell.FOREGROUND
-            #print "FOREGROUND CELL"
 
-    """Returns the boundaries of the cell (left, upper, right, lower)"""
-    def boundaries(self):
-        return (self.x, self.y, self.x + pCell.width, self.y + pCell.height)
-
+        return self.celltype
 
     """Show the cell on screen (probably for debugging"""
     def show(self):
         #im = Image.open(self.filename)
-        im = self.im
-        region = self.boundaries()
+        im = self.im.getColor()
+        region = self.boundaries
         im.crop(region).show()
 
     def cellData(self):
         #im = Image.open(self.filename)
-        im = self.im
-        return im.crop(self.boundaries())
+        im = self.im.getColor()
+        return im.crop(self.boundaries)
 
     def histogram(self):
-        lum_hist = self.cellData().convert("L").histogram()
+        data = self.im.getBW().crop(self.boundaries)
+        lum_hist = data.histogram()
         return numpy.array(lum_hist)
