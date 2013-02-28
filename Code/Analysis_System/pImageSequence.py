b6fa99c7d571b8a6ab6507a1d90e72a5cfb5dc51
pes014 <pes014@bucknell.edu>
2013-02-28 17:47:11 -0500

    Key image detection working. Updated worklog

--- Code/Analysis_System/pImageSequence.py
+++ Code/Analysis_System/pImageSequence.py
@@ -3,6 +3,7 @@ import pCell
 from pCell import *
 import glob
 import numpy
+from pImgMgr import pImgMgr
 
 class pImageSequence(object):
 
@@ -14,10 +15,74 @@ class pImageSequence(object):
 
         self.histograms = [[numpy.zeros(256) for i in xrange(self.cpc)] for i in xrange(self.cpr)]
 
-        for jpg in glob.glob("%s/*.jpg" % self.directory):
+        jpgs = glob.glob("%s/*.jpg" % self.directory)
+        default = pImage(jpgs[0])
+        default.makeDefault()
+
+        
+        for jpg in jpgs:
             pImg = pImage(jpg)
             pImg.cellify(self.cpr, self.cpc)
             self.images.append(pImg)
+
+    @staticmethod
+    def cert(cellForegroundPct, imageForegroundPct):
+        a = 0.5
+        cert = (1-a)*cellForegroundPct + a * imageForegroundPct
+        return 1 - cert
+
+    def findKeyImages(self):
+        # Setup key image 
+        keyImageCount = 0
+        keyImage = pImage(self.images[0].filename)
+        keyImage.cellify(self.cpr, self.cpc)
+        keyImage.classifyCells(pCell.BOARD)
+
+        maxDevPct = 10                   # Threshold for identifying key images
+        decreasingInformation = False   # If stroke cells are becoming board cells
+        debugCount = 1                  # Used to generate debugging images
+        
+        for image in self.images:
+            
+            # Check to see how many stroke cells are becoming board cells
+            nFewerStrokes = 0
+            for x in xrange(self.cpr):
+                for y in xrange(self.cpc):
+                    if keyImage.cellAt(x,y).celltype == pCell.STROKE and image.cellAt(x,y).celltype == pCell.BOARD:
+                        nFewerStrokes += 1;
+
+            # Calculate deviation of stroke cells
+            deviation = nFewerStrokes * 100.0 / (keyImage.strokeCount + 1)
+            print "Deviation %s decreasing %s" %(deviation, decreasingInformation)
+
+            # If more board cells and not mid-erase then key image
+            if deviation >= maxDevPct and not decreasingInformation:
+                print "Saving images with %s fewer strokes (%s)" % (nFewerStrokes, deviation)
+                keyImage.save("./test_images/Out/keyimg%s.jpg" % keyImageCount)
+                keyImageCount += 1
+                decreasingInformation = True
+
+            elif deviation < maxDevPct:
+                decreasingInformation = False
+
+
+
+            # Cut and paste necessary cells
+            for x in xrange(self.cpr):
+                for y in xrange(self.cpc):
+                    imCellType = image.cellAt(x,y).celltype
+                    keyCellType = keyImage.cellAt(x,y).celltype
+
+                    if imCellType != pCell.FOREGROUND:
+                        cell = image.cellAt(x,y)
+                        keyImage.pasteCell(cell, x, y)
+
+            keyImage.save("./test_images/Out/debug%s.jpg" % debugCount)
+            debugCount += 1
+            image.free()
+
+
+        keyImage.save("./test_images/Out/keyimg%s.jpg" % keyImageCount)
 
     def generateLuminance(self):
         
@@ -28,9 +93,9 @@ class pImageSequence(object):
             count += 1
             for x in xrange(self.cpr):
                 for y in xrange(self.cpc):
-                    hist = image.cells[x][y].histogram()
+                    hist = image.cellAt(x, y).histogram()
                     self.histograms[x][y] += hist
-        image.im.free()
+            image.free()
 
         for x in xrange(self.cpr):
             for y in xrange(self.cpc):
@@ -46,8 +111,8 @@ class pImageSequence(object):
         i = 1
         for image in self.images:
             image.classifyCells()
-            image.im.save("./test_images/Out/out%.2d.jpg" % i)
-            image.im.free()
+            image.save("./test_images/Out/out%.2d.jpg" % i)
+            image.free()
             
             print "Finished image %s of %s" % (i, len(self.images))
             i = i + 1
